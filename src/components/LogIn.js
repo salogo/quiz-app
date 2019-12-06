f8ef26ba3190b3dca39f44a84a7fe4cd3e8622d4
salogo <54459398+salogo@users.noreply.github.com>
2019-12-06 16:08:47 -0600

    commit message

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,11 +1,31 @@
 import React from "react";
+import {Form ,Button } from "react-bootstrap";
+
 
 
 const LogIn =()=> {
     return (
-      <div >
-      log In
-      </div>
+       <div>
+<Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Sgn-In</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Log In
+  </Button>
+</Form>
+
+       </div>
     );
   }
   
